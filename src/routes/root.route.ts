3d7aa0f30dfd9dcf36d7c840632eb2fa77842be2
Pitta santhosh kumar <79613980+santhoshkumar43@users.noreply.github.com>
2025-01-16 13:01:51 +0000

    removed WebSocket server with MongoDB is running!,,, insteda made it console log

--- src/routes/root.route.ts
+++ src/routes/root.route.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("WebSocket server with MongoDB is running!");
+  console.log("WebSocket server with MongoDB is running!");
 });
 
 export default router;
