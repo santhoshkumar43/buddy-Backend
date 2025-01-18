9ade9ff5a18aad465c6425630b6336442ad5396b
Pitta santhosh kumar <79613980+santhoshkumar43@users.noreply.github.com>
2025-01-18 11:41:16 +0000

    made a small change in port code snipet

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { initializeWebSocketServer } from "./websocket/websocket.server";
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Create HTTP server
 const server = createServer(app);
